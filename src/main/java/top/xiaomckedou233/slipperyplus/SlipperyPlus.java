40cee9326abd697955cdc6ff3e39d684b10f2a0a
Xiaomckedou233 <xiaomckedou@gmail.com>
2023-08-05 14:18:26 +0800

    Super Slippery!

--- src/main/java/top/xiaomckedou233/slipperyplus/SlipperyPlus.java
+++ src/main/java/top/xiaomckedou233/slipperyplus/SlipperyPlus.java
@@ -1,2 +1,15 @@
-public class SlipperyPlus {
+package top.xiaomckedou233.slipperyplus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlipperyPlus implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("slipperyplus");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("SlipperyPlus,Start!");
+    }
 }
--- src/main/java/top/xiaomckedou233/slipperyplus/mixin/BlockMixin.java
+++ src/main/java/top/xiaomckedou233/slipperyplus/mixin/BlockMixin.java
@@ -1,2 +1,20 @@
-package top.xiaomckedou233.slipperyplus.mixin;public class BlockMixin {
+package top.xiaomckedou233.slipperyplus.mixin;
+
+import net.minecraft.block.Block;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public abstract class BlockMixin {
+    @Inject(method = "getSlipperiness", at = @At("HEAD"), cancellable = true)
+    private void injectSlipperiness(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(0.989F);
+    }
+
+    @Inject(method = "getVelocityMultiplier", at = @At("HEAD"), cancellable = true)
+    private void injectVelocityMultiplier(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(1.1F);
+    }
 }
